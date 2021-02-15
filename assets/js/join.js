2f994bcd19abb812b9721f16f384eb33b803f204
William <69987571+AviaHumble@users.noreply.github.com>
2021-02-15 14:43:05 +0000

    Add files via upload

---
+++ assets/js/join.js
@@ -0,0 +1,51 @@
+function joinOryx(){
+    setTimeout(function(){
+    }, 2000)
+    const element = document.getElementById('button-tfjoin')
+        element.classList.remove('btn-info')
+        element.classList.add('btn-success')
+    setTimeout(function(){
+        window.location.href = 'https://discord.gg/69zC23ZEsH'
+    }, 2000)
+}
+function joinOryx2(){
+    setTimeout(function(){
+    }, 2000)
+    const element = document.getElementById('button-tfjoin2')
+        element.classList.remove('btn-info')
+        element.classList.add('btn-success')
+    setTimeout(function(){
+        window.location.href = 'https://discord.gg/69zC23ZEsH'
+    }, 2000)
+}
+function joinOryx3(){
+    setTimeout(function(){
+    }, 2000)
+    const element = document.getElementById('button-tfjoin3')
+        element.classList.remove('btn-info')
+        element.classList.add('btn-success')
+    setTimeout(function(){
+        window.location.href = 'https://discord.gg/69zC23ZEsH'
+    }, 2000)
+}
+function joinOryx4(){
+    setTimeout(function(){
+    }, 2000)
+    const element = document.getElementById('button-tfjoin4')
+        element.classList.remove('btn-info')
+        element.classList.add('btn-success')
+    setTimeout(function(){
+        window.location.href = 'https://discord.gg/69zC23ZEsH'
+    }, 2000)
+}
+
+function joinOryx5(){
+    setTimeout(function(){
+    }, 2000)
+    const element = document.getElementById('button-tfjoin5')
+        element.classList.remove('btn-info')
+        element.classList.add('btn-success')
+    setTimeout(function(){
+        window.location.href = 'https://discord.gg/69zC23ZEsH'
+    }, 2000)
+}
